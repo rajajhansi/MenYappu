c853ba1f864c192592aa1c5639d0249b274fad25
Bogdan Urs <bogdan.urs@outlook.com>
2016-02-07 05:07:24 +0200

    Made some fixes on the left panel (text issues). Changing the style of the icons should not be easier and should not force you to change styles of the adjacent elements. Also added favicons.

--- Tamil.Grammar.Prosody.Parser.Web/Scripts/ui/ui.js
+++ Tamil.Grammar.Prosody.Parser.Web/Scripts/ui/ui.js
@@ -8,15 +8,6 @@
 			throw new Error('please provide an ID for the panel!');
 		}
 
-		var collapsedCssClass = 'panel-collapsed';
-		var collapsedLocalStorageState = 'collapsed';
-		var localStorageIdentifier = 'pagepanel-' + elem.attr('id');
-		var isCollapsedOnLoad = localStorage.getItem(localStorageIdentifier) === collapsedLocalStorageState;
-
-		if (isCollapsedOnLoad) {
-			elem.addClass(collapsedCssClass);
-		}
-
 		// defaults
 		var defaults = {
 			toggler: elem.find('.stacked-menus-toggler'),
@@ -27,17 +18,45 @@
 		// overwriting the defaults if we provide the params
 		var config = $.extend(defaults, params);
 
+		//TODO[burs]: This will need some refactoring if we're gonna have multiple panels
+
+		var collapsedCssClass = 'panel-collapsed';
+		var collapsedLocalStorageState = 'collapsed';
+		var localStorageIdentifier = 'pagepanel-' + elem.attr('id');
+		var isCollapsedOnLoad = localStorage.getItem(localStorageIdentifier) === collapsedLocalStorageState;
+
+		if (config.onLoad) {
+			config.onLoad(isCollapsedOnLoad, elem);
+		}
+
+		if (isCollapsedOnLoad) {
+			elem.addClass(collapsedCssClass);
+		}
+
 		config.toggler.on('click', function (e) {
 			if (elem.hasClass(collapsedCssClass)) {
+				if (config.beforeExpand) {
+					config.beforeExpand(e, elem);
+				}
+
 				elem.removeClass(collapsedCssClass);
 				localStorage.removeItem(localStorageIdentifier);
+				
+				if (config.afterExpand) {
+					config.afterExpand(e, elem);
+				}
 			} else {
+				if (config.beforeCollapse) {
+					config.beforeCollapse(e, elem);
+				}
+
 				elem.addClass(collapsedCssClass);
 				localStorage.setItem(localStorageIdentifier, collapsedLocalStorageState);
+
+				if (config.afterCollapse) {
+					config.afterCollapse(e, elem);
+				}
 			}
 		});
 	};
-
-	$("#container-wrapper-with-panels").panelExpander();
-
 })(jQuery);
