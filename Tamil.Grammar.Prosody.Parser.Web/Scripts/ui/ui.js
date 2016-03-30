d519aed7f47fccb5814e513a9f1b74965067aa25
Bogdan Urs <bogdan.urs@outlook.com>
2016-03-30 18:51:16 +0300

    added chevron for the submenu

--- Tamil.Grammar.Prosody.Parser.Web/Scripts/ui/ui.js
+++ Tamil.Grammar.Prosody.Parser.Web/Scripts/ui/ui.js
@@ -1,87 +1,96 @@
-(function($){
+(function ($) {
 
-	$.fn.panelExpander = function (params) {
-		var elem = $(this);
-		var elemId = elem.attr('id');
+    $.fn.panelExpander = function (params) {
+        var elem = $(this);
+        var elemId = elem.attr('id');
 
-		if (!$.trim(elemId)) {
-			throw new Error('please provide an ID for the panel!');
-		}
+        if (!$.trim(elemId)) {
+            throw new Error('please provide an ID for the panel!');
+        }
 
-		// defaults
-		var defaults = {
-			toggler: elem.find('.stacked-menus-toggler'),
-			leftPanel: elem.find('.left-panelbar'),
-			rightPanel: elem.find('.right-panelbar')
-		};
+        // defaults
+        var defaults = {
+            toggler: elem.find('.stacked-menus-toggler'),
+            leftPanel: elem.find('.left-panelbar'),
+            rightPanel: elem.find('.right-panelbar')
+        };
 
-		// overwriting the defaults if we provide the params
-		var config = $.extend(defaults, params);
+        // overwriting the defaults if we provide the params
+        var config = $.extend(defaults, params);
 
-		//TODO[burs]: This will need some refactoring if we're gonna have multiple panels
+        //TODO[burs]: This will need some refactoring if we're gonna have multiple panels
 
-		var collapsedCssClass = 'panel-collapsed';
-		var collapsedLocalStorageState = 'collapsed';
-		var localStorageIdentifier = 'pagepanel-' + elem.attr('id');
-		var isCollapsedOnLoad = localStorage.getItem(localStorageIdentifier) === collapsedLocalStorageState;
+        var collapsedCssClass = 'panel-collapsed';
+        var collapsedLocalStorageState = 'collapsed';
+        var localStorageIdentifier = 'pagepanel-' + elem.attr('id');
+        var isCollapsedOnLoad = localStorage.getItem(localStorageIdentifier) === collapsedLocalStorageState;
 
-		if (config.onLoad) {
-			config.onLoad(isCollapsedOnLoad, elem);
-		}
+        if (config.onLoad) {
+            config.onLoad(isCollapsedOnLoad, elem);
+        }
 
-		if (isCollapsedOnLoad) {
-			elem.addClass(collapsedCssClass);
-		}
+        if (isCollapsedOnLoad) {
+            elem.addClass(collapsedCssClass);
+        }
 
-		config.toggler.on('click', function (e) {
-			if (elem.hasClass(collapsedCssClass)) {
-				if (config.beforeExpand) {
-					config.beforeExpand(e, elem);
-				}
+        config.toggler.on('click', function (e) {
+            if (elem.hasClass(collapsedCssClass)) {
+                if (config.beforeExpand) {
+                    config.beforeExpand(e, elem);
+                }
 
-				elem.removeClass(collapsedCssClass);
-				localStorage.removeItem(localStorageIdentifier);
-				
-				if (config.afterExpand) {
-					config.afterExpand(e, elem);
-				}
-			} else {
-				if (config.beforeCollapse) {
-					config.beforeCollapse(e, elem);
-				}
+                elem.removeClass(collapsedCssClass);
+                localStorage.removeItem(localStorageIdentifier);
 
-				elem.addClass(collapsedCssClass);
-				localStorage.setItem(localStorageIdentifier, collapsedLocalStorageState);
+                if (config.afterExpand) {
+                    config.afterExpand(e, elem);
+                }
+            } else {
+                if (config.beforeCollapse) {
+                    config.beforeCollapse(e, elem);
+                }
 
-				if (config.afterCollapse) {
-					config.afterCollapse(e, elem);
-				}
-			}
-		});
-	};
+                elem.addClass(collapsedCssClass);
+                localStorage.setItem(localStorageIdentifier, collapsedLocalStorageState);
 
-	$.fn.panelSubmenus = function (params) {
-		var elem = $(this);
+                if (config.afterCollapse) {
+                    config.afterCollapse(e, elem);
+                }
+            }
+        });
+    };
 
-		var defaults = {
-			menus: elem.find(".stacked-menus-items"),
-			submenuClass: 'stacked-menus-items-submenu'
-		};
+    $.fn.panelSubmenus = function (params) {
+        var elem = $(this);
 
-		var config = $.extend(defaults, params);
+        var defaults = {
+            menus: elem.find(".stacked-menus-items"),
+            submenuClass: 'stacked-menus-items-submenu'
+        };
 
-		config.menus.each(function (idx, val) {
-			var currMenuElem = $(val);
-			var hasSubmenu = currMenuElem.find('.' + config.submenuClass).length > 0;
-			if (hasSubmenu) {
-				currMenuElem.addClass('with-submenu');
-			}
-		});
+        var config = $.extend(defaults, params);
 
-		elem.on('click', '.with-submenu > a', function (e) {
-			$(e.currentTarget).closest('.with-submenu').toggleClass('expanded');
-			e.preventDefault();
-		});
-	};
+        config.menus.each(function (idx, val) {
+            var currMenuElem = $(val);
+            var hasSubmenu = currMenuElem.find('.' + config.submenuClass).length > 0;
+            if (hasSubmenu) {
+                var chevron = $("<span></span>");
+                chevron.addClass('subitems-toggler glyph glyph-chevron-down');
+
+                currMenuElem.addClass('with-submenu')
+                    .append(chevron);
+            }
+        });
+
+        elem.on('click', '.with-submenu > a', function (e) {
+            $(e.currentTarget).closest('.with-submenu')
+                .toggleClass('expanded')
+                .children('.subitems-toggler')
+                .toggleClass('glyph-chevron-down')
+                .toggleClass('glyph-chevron-up');
+
+            e.preventDefault();
+        });
+    };
 
 })(jQuery);
