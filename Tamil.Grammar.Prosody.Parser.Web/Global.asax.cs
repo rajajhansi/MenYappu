6fb71894836c175c597208aae358fc01a901e746
Raja Mani <rmani@transperfect.com>
2016-04-16 11:01:31 -0700

    Removed unwanted authentiction and OAuth files to deploy into Azure

--- Tamil.Grammar.Prosody.Parser.Web/Global.asax.cs
+++ Tamil.Grammar.Prosody.Parser.Web/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Http;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -16,13 +11,9 @@ namespace Tamil.Parody.Parser.Web
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
